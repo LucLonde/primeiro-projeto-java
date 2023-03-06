be3ddd3aeed0f0d1ba6d6c28408ebe1a1d2c20a0
LucLonde <lucasro32@hotmail.com>
2023-03-06 19:30:25 -0300

    variavel alterada para final

--- src/Beecrowd/03março/AreaCircunferencia.java
+++ src/Beecrowd/03março/AreaCircunferencia.java
@@ -10,10 +10,10 @@ public class AreaCircunferencia {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        double pi = 3.14159;
+        final double PI = 3.14159;
         
         double raio = sc.nextDouble();
-        double area = pi * Math.pow(raio, 2);
+        double area = PI * Math.pow(raio, 2);
         
         System.out.printf("A=%.4f%n", area);
         
